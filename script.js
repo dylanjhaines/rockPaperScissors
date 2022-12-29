285009cc26cbe36504ce83ab7254ec918469a93e
Dylan Haines <90574712+dylanjhaines@users.noreply.github.com>
2022-12-29 12:15:29 -0800

    create 5 round game with score

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-/*
-let getComputerChoice = Math.random()
-console.log(getComputerChoice)
-let computerChoice
-if (getComputerChoice <= 0.33) {
-    computerChoice = "Rock"
-} else if (getComputerChoice > 0.666666) {
-    computerChoice = "Paper"
-} else computerChoice = "Scissors"
-*/
 
-const computerSelection = getComputerChoice();
 
 function getComputerChoice() {
     let choice = Math.random()
@@ -44,9 +33,45 @@ function playRound(playerSelection, computerSelection) {
         return "Winner! Scissors beats paper.";
     } else if (computerSelection === "Scissors" && playerSelection === "scissors") {
         return "Tie! Scissors ties Scissors.";
-    } 
+    } else {
+        return "Something went wrong, make sure you're spelling correctly!"
+    }
   }
-   
-  const playerSelectionInput = "Rock";
-  const playerSelection = playerSelectionInput.toLowerCase();
-  console.log(playRound(playerSelection, computerSelection));
+  let playerSelection
+  let computerSelection
+
+  function game() {
+    let win = 0;
+    let lose = 0;
+    let tie = 0;
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerChoice();
+        let playerSelectionInput = prompt("Choose Rock, Paper, or Scissors");
+        let playerSelection = playerSelectionInput.toLowerCase();
+        playRound(playerSelection, computerSelection);
+        if (computerSelection === "Rock" && playerSelection === "rock") {
+            tie += 1;
+        } else if (computerSelection === "Paper" && playerSelection === "rock") {
+            lose += 1;
+        } else if (computerSelection === "Scissors" && playerSelection === "rock") {
+            win += 1;
+        } else if (computerSelection === "Rock" && playerSelection === "paper") {
+            win += 1;
+        } else if (computerSelection === "Paper" && playerSelection === "paper") {
+            tie += 1;
+        } else if (computerSelection === "Scissors" && playerSelection === "paper") {
+            lose += 1;
+        } else if (computerSelection === "Rock" && playerSelection === "scissors") {
+            lose += 1;
+        } else if (computerSelection === "Paper" && playerSelection === "scissors") {
+            win += 1;
+        } else if (computerSelection === "Scissors" && playerSelection === "scissors") {
+            tie += 1;
+        } else {
+            return "Something went wrong, make sure you're spelling correctly!"
+        }
+        console.log(playRound(playerSelection, computerSelection) + "  Your record is: " + win + "-" + lose + "-" + tie);
+    }
+  }
+
+ console.log(game());
